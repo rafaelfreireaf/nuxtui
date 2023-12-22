3cc54e7077bb79dfee63ddb3cc7c91de52347f53
Rafael Freire <rafael.freire@arenafantasy.com>
2023-12-22 14:35:59 -0300

    Firebase install

--- app.vue
+++ app.vue
@@ -1,7 +1,45 @@
 <script setup lang="ts">
-const { x, y } = useMouse()
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBa0nUo74uA2w6EMf5Y_iKzBiLwQZUdnJw",
+  authDomain: "vue-notification-4a729.firebaseapp.com",
+  projectId: "vue-notification-4a729",
+  storageBucket: "vue-notification-4a729.appspot.com",
+  messagingSenderId: "372119211975",
+  appId: "1:372119211975:web:ed232e2519b9e99fd953e2",
+  measurementId: "G-NFHCEVQGY5"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+
+// Get registration token. Initially this makes a network call, once retrieved
+// subsequent calls to getToken will return from cache.
+const messaging = getMessaging();
+onMessage(messaging, (payload) => {
+  console.log('Message received. ', payload);
+  // ...
+});
+
+getToken(messaging, { vapidKey: 'BPz5PnaIeRx4ilebd0CYMv-yc-XgPxNe7Grj_0hfucIQaShN5pLq6s-eVuGOBcihtkNZZWNOq6tvxlJu9hDaM0w' }).then((currentToken) => {
+  if (currentToken) {
+    // Send the token to your server and update the UI if necessary
+    console.log('Token is: currentToken')
+    // ...
+  } else {
+    // Show permission request UI
+    console.log('No registration token available. Request permission to generate one.');
+    // ...
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  // ...
+});
 </script>
 
 <template>
-  <div>pos: {{ x }}, {{ y }}</div>
+  <div>Vue Notification</div>
 </template>
---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,37 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+  apiKey: "AIzaSyBa0nUo74uA2w6EMf5Y_iKzBiLwQZUdnJw",
+  authDomain: "vue-notification-4a729.firebaseapp.com",
+  projectId: "vue-notification-4a729",
+  storageBucket: "vue-notification-4a729.appspot.com",
+  messagingSenderId: "372119211975",
+  appId: "1:372119211975:web:ed232e2519b9e99fd953e2",
+  measurementId: "G-NFHCEVQGY5"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    '[firebase-messaging-sw.js] Received background message ',
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
